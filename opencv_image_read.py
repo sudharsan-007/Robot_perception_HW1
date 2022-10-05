adf6a7fb012b1de8a7e91fc195c634484cd8c1c2
sudharsan-007 <55453134+sudharsan-007@users.noreply.github.com>
2022-10-05 06:47:48 -0400

    Add files via upload

---
+++ opencv_image_read.py
@@ -0,0 +1,28 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+import os
+  
+  
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
